7ea2868a96878c8c673b239cc31d43beb887d615
alessanderleite <alessander.logistica@gmail.com>
2019-01-20 00:35:58 -0200

    declare the variables to the ItemDataSource class

--- app/src/main/java/br/com/alessanderleite/androidpaginglibrary/ItemDataSource.java
+++ app/src/main/java/br/com/alessanderleite/androidpaginglibrary/ItemDataSource.java
@@ -5,6 +5,15 @@ import android.support.annotation.NonNull;
 
 public class ItemDataSource extends PageKeyedDataSource<Integer, Item> {
 
+    //the size of a page that we want
+    public static final int PAGE_SIZE = 50;
+
+    //we will start from the first page which is 1
+    public static final int FIRST_PAGE = 1;
+
+    //we need to fetch for stackoverflow
+    public static final String SITE_NAME = "stackoverflow";
+
     @Override
     public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull LoadInitialCallback<Integer, Item> callback) {
     }

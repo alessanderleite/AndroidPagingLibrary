e26060c1a6c4b1c72f669459f73c73fc795e83fe
alessanderleite <alessander.logistica@gmail.com>
2019-01-20 10:51:14 -0200

    code the loadInitial, loadBefore and the loadAfter methods in the ItemDataSource class

--- app/src/main/java/br/com/alessanderleite/androidpaginglibrary/ItemDataSource.java
+++ app/src/main/java/br/com/alessanderleite/androidpaginglibrary/ItemDataSource.java
@@ -3,6 +3,10 @@ package br.com.alessanderleite.androidpaginglibrary;
 import android.arch.paging.PageKeyedDataSource;
 import android.support.annotation.NonNull;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ItemDataSource extends PageKeyedDataSource<Integer, Item> {
 
     //the size of a page that we want
@@ -14,17 +18,77 @@ public class ItemDataSource extends PageKeyedDataSource<Integer, Item> {
     //we need to fetch for stackoverflow
     public static final String SITE_NAME = "stackoverflow";
 
+    //this will be called once to load the initial data
     @Override
-    public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull LoadInitialCallback<Integer, Item> callback) {
+    public void loadInitial(@NonNull LoadInitialParams<Integer> params, @NonNull final LoadInitialCallback<Integer, Item> callback) {
+        RetrofitClient.getInstance()
+                .getApi().getAnswers(FIRST_PAGE, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
+                        if (response.body() !=  null) {
+                            callback.onResult(response.body().items, null, FIRST_PAGE + 1);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
+    }
+
+    //this will load the previous page
+    @Override
+    public void loadBefore(@NonNull final LoadParams<Integer> params, @NonNull final LoadCallback<Integer, Item> callback) {
+        RetrofitClient.getInstance()
+                .getApi().getAnswers(params.key, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
+
+                        //if the current page is greater than one
+                        //we are decrementing the page number
+                        //else there is no previous page
+                        Integer adjacentKey = (params.key > 1) ? params.key - 1 : null;
+                        if (response.body() != null) {
+
+                            //passing the loaded data
+                            //and the previous page key
+                            callback.onResult(response.body().items, adjacentKey);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
     }
 
     @Override
-    public void loadBefore(@NonNull LoadParams<Integer> params, @NonNull LoadCallback<Integer, Item> callback) {
+    public void loadAfter(@NonNull final LoadParams<Integer> params, @NonNull final LoadCallback<Integer, Item> callback) {
+        RetrofitClient.getInstance()
+                .getApi()
+                .getAnswers(params.key, PAGE_SIZE, SITE_NAME)
+                .enqueue(new Callback<StackApiResponse>() {
+                    @Override
+                    public void onResponse(Call<StackApiResponse> call, Response<StackApiResponse> response) {
 
-    }
+                        if (response.body() !=null) {
+                            //if the response has next page
+                            //incrementing the next page number
+                            Integer key = response.body().has_more ? params.key + 1 : null;
 
-    @Override
-    public void loadAfter(@NonNull LoadParams<Integer> params, @NonNull LoadCallback<Integer, Item> callback) {
+                            //passing the loaded data and next page value
+                            callback.onResult(response.body().items, key);
+                        }
+                    }
 
+                    @Override
+                    public void onFailure(Call<StackApiResponse> call, Throwable t) {
+
+                    }
+                });
     }
 }
